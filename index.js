33d28bb09f34515a22367b0963ba92748e275afb
SenalFF <mrsenalff@gmail.com>
2025-06-13 05:21:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const axios = require('axios');
 const { File } = require('megajs');
 const prefix = '.';
 
-const ownerNumber = ['94767707223'];
+const ownerNumber = ['94769872326'];
 
 //===================SESSION-AUTH============================
 if (!fs.existsSync(__dirname + '/auth_info_baileys/creds.json')) {
