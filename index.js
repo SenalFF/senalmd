dd0e69249268fe0bc47e7b99c264be194adb6092
SenalFF <mrsenalff@gmail.com>
2025-10-27 19:11:18 -0700

    Update index.js

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ END:VCARD`
 };
 
 // ================= Owner =================
-const ownerNumber = [config.OWNER_NUMBER || "94712872326"];
+const ownerNumber = [config.OWNER_NUMBER || "94769872326"];
 
 //=================== SESSION AUTH ============================
 const authPath = __dirname + "/auth_info_baileys";
